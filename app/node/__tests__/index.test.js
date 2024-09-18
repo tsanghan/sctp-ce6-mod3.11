f3641fdc6311738087e0a73073c4a5d7c55fbedf
Wong Tsang Han <tsanghan@gmail.com>
2024-09-18 12:02:28 +0800

    fixed unit test

--- app/node/__tests__/index.test.js
+++ app/node/__tests__/index.test.js
@@ -8,16 +8,16 @@ afterAll((done) => {
 })
 
 describe('Express App', () => {
-  test('GET / should return 200 and "Hello from Node 4!"', async () => {
+  test('GET / should return 200 and "Hello from Node 4! for Mod3.11"', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('Hello from Node 4!');
+    expect(response.text).toBe('Hello from Node 4! for Mod3.11');
   });
 
-  test('GET /test should return 200 and "Hello from /test Node!"', async () => {
+  test('GET /test should return 200 and "Hello from /test Node! for Mod3.11"', async () => {
     const response = await request(app).get('/test');
     expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('Hello from /test Node!');
+    expect(response.text).toBe('Hello from /test Node! for Mod3.11');
   });
 
   test('GET /unknown should return 404', async () => {
