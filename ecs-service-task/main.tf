9d6388f5443990032df57e29b0c2c9caa45ba633
Wong Tsang Han <tsanghan@gmail.com>
2024-09-18 15:07:58 +0800

    fix aws_iam_role ref

--- ecs-service-task/main.tf
+++ ecs-service-task/main.tf
@@ -87,7 +87,7 @@ resource "aws_ecs_task_definition" "tsanghan-ce6" {
   network_mode             = "awsvpc"
   cpu                      = 256
   memory                   = 512
-  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
